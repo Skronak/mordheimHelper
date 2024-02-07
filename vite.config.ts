b71e63bd37f606030a0ba9c4997a970c32e2f82a
skronak <narvak.skronak@gmail.com>
2024-02-07 21:37:39 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
-    base: "/mordheimHelper/",
+    base: "https://skronak.github.io/mordheimHelper/",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
